2f9d9789e3814fe2016f0822d34bcb07bdc6cf88
Marshall Smith <marshallsmithxv8@gmail.com>
2021-09-04 04:28:04 +0900

    Добавьте файлы проекта.

---
+++ stella/stella.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+
+int main()
+{
+	using std::cout;
+	using std::endl;
+
+	cout << "Hella sup!" << endl;
+	cout << "pizda" << endl;
+
+	int g, z, f;
+	g = 7;
+	z = 6;
+	f = (g + z) * 3;
+	std::cout << f << std::endl;
+	std::cout << "when i grow up" << std::endl;
+	
+	int a, b;
+	a = 5;
+	b = 6;
+	a = b;
+	b = b + a;
+	cout << b << endl;
+
+	return 0;
+
+}
